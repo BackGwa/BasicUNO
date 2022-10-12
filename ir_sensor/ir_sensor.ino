453c994fb5696143bacf83dc7fb2a093beaa45e6
BackGwa <64456214+BackGwa@users.noreply.github.com>
2022-10-12 09:09:19 +0900

    IR 감지 함수

---
+++ ir_sensor/ir_sensor.ino
@@ -0,0 +1,16 @@
+
+#define IR_SENSOR 0
+
+bool isIR(int IR_SENSOR_PIN = IR_SENSOR);
+
+/*
+void setup() {...}
+void loop() {...}
+*/
+
+bool isIR(int IR_SENSOR_PIN = IR_SENSOR)
+{
+
+  return (digitalRead(IR_SENSOR_PIN) == 1) ? true : false;
+
+}
